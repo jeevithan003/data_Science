c86d4864f81acdf5bf9baba071e37d0673b71b50
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-22 23:04:43 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,8 @@
+import numpy as np
+Calorie_burnage = [240, 250, 260, 270, 280, 290, 300, 310, 320, 330]
+Average_calorie_burnage1 = np.max(Calorie_burnage)
+Average_calorie_burnage2 = np.min(Calorie_burnage)
+Average_calorie_burnage3 = np.mean(Calorie_burnage)
+print(Average_calorie_burnage1)
+print(Average_calorie_burnage2)
+print(Average_calorie_burnage3)
