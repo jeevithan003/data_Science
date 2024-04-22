cdfa12d54e1b9007ac3a24eae2e08c0d784fb3e4
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-22 23:00:43 +0530

    Create row_and_col.py

---
+++ row_and_col.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+d = {'col1': [1, 2, 3, 4, 7], 'col2': [4, 5, 6, 9, 5], 'col3': [7, 8, 12, 1, 11]}
+
+df = pd.DataFrame(data=d)
+#counting row and rows and col in using range 
+col=df.shape[1]
+print(col)
